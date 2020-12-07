6c00afd48d9b21c6ba1e78729fff25a55d32dcbd
nparashar150 <nparashar150@gmail.com>
2020-12-07 14:43:25 +0530

    delay bouncer

--- assets/code/js/scripts.js
+++ assets/code/js/scripts.js
@@ -1,7 +1,7 @@
 (function($) {
     "use strict"; 	
 	$(window).on('load', function() {
-		var preloaderFadeOutTime = 200;
+		var preloaderFadeOutTime = 500;
 		function hidePreloader() {
 			var preloader = $('.spinner-wrapper');
 			setTimeout(function() {
