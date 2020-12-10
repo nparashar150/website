d6be0be01cbcd53bf041637142e69b2b622481cd
nparashar150 <nparashar150@gmail.com>
2020-12-10 17:42:39 +0530

    Added Navbar

--- assets/code/js/scripts.js
+++ assets/code/js/scripts.js
@@ -84,4 +84,12 @@
         navbar.classList.remove("sticky");
       }
     }
+    function myFunction() {
+        var x = document.getElementById("myNavbar");
+        if (x.className === "navbar") {
+          x.className += " responsive";
+        } else {
+          x.className = "navbar";
+        }
+      }
 })(jQuery);
